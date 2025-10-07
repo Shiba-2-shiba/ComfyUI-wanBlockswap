7074a33da48027f6b40757de351d670cc82512da
Shiba*2 <135475967+Shiba-2-shiba@users.noreply.github.com>
2025-10-07 10:36:54 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 from .nodes import NODE_CLASS_MAPPINGS as wan_class_mappings, NODE_DISPLAY_NAME_MAPPINGS as wan_display_mappings
 
 # 新しく追加したmemory_nodes.pyからマッピングをインポート
-from .memory_nodes import NODE_CLASS_MAPPINGS as mem_class_mappings, NODE_DISPLAY_NAME_MAPPINGS as mem_display_mappings
+from .MemoryUtils import NODE_CLASS_MAPPINGS as mem_class_mappings, NODE_DISPLAY_NAME_MAPPINGS as mem_display_mappings
 
 # 2つのCLASSマッピングを結合
 NODE_CLASS_MAPPINGS = {**wan_class_mappings, **mem_class_mappings}
